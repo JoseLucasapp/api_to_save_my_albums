0d20cdd0a32d9dac70c2657e5b1ea6e33be482ec
joselucasapp <josluc2016@gmail.com>
2021-07-22 11:59:27 -0300

    feat: create the project server

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3333;
+
+//Middlewares
+
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
+app.use(cors());
+
+//Routes
+const router = express.Router();
+app.use('/api', router);
+require('./routes')(router);
+
+app.listen(PORT);
